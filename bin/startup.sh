9244dce8d6114825dc47d479b9945b0055ef0321
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2013-08-20 11:42:33 +0200

    Initial Commit

---
+++ bin/startup.sh
@@ -0,0 +1,6 @@
+#Oracle environment
+export ORACLE_BASE=/opt/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/11.2.0.3/db
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+
+nohup /usr/bin/python ./pagent -c ../etc/pagent.ini &
