20267a2b0c1c91d21c8662418860ffce59829aca
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2013-09-27 11:40:59 +0200

    Renamed pagent.ini to pagent.conf

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,4 +3,4 @@ export ORACLE_BASE=/opt/oracle
 export ORACLE_HOME=$ORACLE_BASE/product/11.2.0.3/db
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib
 
-nohup /usr/bin/python ./pagent -c ../etc/pagent.ini &
+nohup /usr/bin/python ./pagent -c ../etc/pagent.conf &
