56c8b08bd8ac86c70a882e605d230bb80fc67c53
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2013-10-07 13:03:13 +0200

    Removed env.sh

--- bin/env.sh
+++
@@ -1,6 +0,0 @@
-#MIOS-AGENT environment
-export MAGENT_HOME=/opt/mios/mios-agent
-#Oracle environment
-export ORACLE_BASE=/opt/oracle
-export ORACLE_HOME=$ORACLE_BASE/product/11.2.0.3/db
-export LD_LIBRARY_PATH=$ORACLE_HOME/lib
